1069529f6e040f7b0dce89a99ddaee8f3b06cce4
Kyle J. Harms <kyle.harms@cornell.edu>
2023-09-21 09:47:32 -0400

    pass dark mode down

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,19 @@ export default function App() {
     <div style={{ backgroundColor: (isDarkMode ? 'black' : 'white') }}>
       <DarkModeSwitcher onSwitchDarkMode={setIsDarkMode} />
 
-      <AccordionPanel title="Props">
+      <AccordionPanel title="Props" isDarkMode={isDarkMode}>
         React components use props to communicate with each other. Every parent component can pass some information to its child components by giving them props. Props might remind you of HTML attributes, but you can pass any JavaScript value through them, including objects, arrays, and functions.
       </AccordionPanel>
 
-      < AccordionPanel title="State">
+      < AccordionPanel title="State" isDarkMode={isDarkMode}>
         Components often need to change what’s on the screen as a result of an interaction. Typing into the form should update the input field, clicking “next” on an image carousel should change which image is displayed, clicking “buy” should put a product in the shopping cart. Components need to “remember” things: the current input value, the current image, the shopping cart. In React, this kind of component-specific memory is called state.
       </AccordionPanel>
 
-      < AccordionPanel title="Events">
+      < AccordionPanel title="Events" isDarkMode={isDarkMode}>
         React lets you add event handlers to your JSX. Event handlers are your own functions that will be triggered in response to interactions like clicking, hovering, focusing form inputs, and so on.
       </AccordionPanel>
 
-      < AccordionPanel title="Lifting State Up">
+      < AccordionPanel title="Lifting State Up" isDarkMode={isDarkMode}>
         Sometimes, you want the state of two components to always change together. To do it, remove state from both of them, move it to their closest common parent, and then pass it down to them via props. This is known as lifting state up, and it’s one of the most common things you will do writing React code.
       </AccordionPanel>
     </div>
--- src/components/AccordionPanel.jsx
+++ src/components/AccordionPanel.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react';
 
 import './AccordionPanel.css';
 
-export default function AccordionPanel({ title, children }) {
+export default function AccordionPanel({ title, isDarkMode, children }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <section>
-      <h3 onClick={() => setIsExpanded(!isExpanded)}>
+      <h3 onClick={() => setIsExpanded(!isExpanded)} style={{ color: (isDarkMode ? 'white' : 'black') }}>
         {(isExpanded ? '⮟' : '⮞') + ' ' + title}
       </h3>
 
-      {isExpanded && (<p>{children}</p>)}
+      {isExpanded && (<p style={{ color: (isDarkMode ? 'white' : 'black') }}>{children}</p>)}
     </section>
   );
 }
